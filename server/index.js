d2f5464e9115583618e7fbf90f9c4b27ead23a83
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-04-16 10:57:45 -0600

    basic server setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,43 @@
-//TODO Basic express setup
+const express = require('express')
+const massive = require('massive')
+require('dotenv').config()
+const session = require('express-session')
+const postCtrl = require('./controller')
+const authCtrl = require('./authController')
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 
-//#auth endpoints
-//TODO login, register, logout, getUser
+const app = express()
 
-//#posts endpoints
-//TODO get post put delete posts
+app.use(express.json())
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+    secret: SESSION_SECRET,
+  })
+)
+//# Auth endpoints
+app.post('/auth/login', authCtrl.login)
+app.post('/auth/register', authCtrl.register)
+app.delete('/auth/logout', authCtrl.logout)
+app.get('/auth/user', authCtrl.getUser)
+
+//# Posts endpoints
+app.get('/api/posts', postCtrl.getPosts)
+app.post('/api/posts', postCtrl.addPost)
+app.put('/api/posts/:id', postCtrl.editPost)
+app.delete('/api/posts/:id', postCtrl.deletePost)
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((dbInstance) => {
+  app.set('db', dbInstance)
+  console.log('Silly db, trix are for kids')
+  app.listen(SERVER_PORT, () =>
+    console.log(`Crunchatize me cap'n on port ${SERVER_PORT}`)
+  )
+})
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import AuthHeader from './Components/AuthHeader'
-import Profile from './Components/Profile'
+import Landing from './Components/Landing'
 import './App.css'
 
 function App(props) {
   return (
     <div className="App">
       <AuthHeader />
-      <Profile />
+      <Landing />
     </div>
   )
 }
