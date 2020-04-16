92e2e975b0e41077d084fbc3c4cd824eaa1f41b9
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-04-16 11:58:54 -0600

    Auth done

--- server/authController.js
+++ server/authController.js
@@ -1,16 +1,59 @@
 const bcrypt = require('bcryptjs')
 
 module.exports = {
-  login: () => {
-    //TODO Login existing user
+  login: async (req, res) => {
+    const db = req.app.get('db')
+    const { email, password } = req.body
+
+    const existingUser = await db.check_user([email])
+
+    if (!existingUser[0]) {
+      return res.status(404).send('User does not exist')
+    }
+
+    const authenticated = bcrypt.compareSync(password, existingUser[0].password)
+
+    if (authenticated) {
+      delete existingUser[0].password
+
+      req.session.user = existingUser[0]
+
+      res.status(200).send(req.session.user)
+    } else {
+      res.status(403).send('Email or password incorrect')
+    }
   },
-  register: () => {
-    //TODO Register new user
+  register: async (req, res) => {
+    const db = req.app.get('db')
+    const { email, password } = req.body
+
+    // db.check_user([email]).then(existingUser => {
+
+    // })
+    const existingUser = await db.check_user([email])
+
+    if (existingUser[0]) {
+      return res.status(409).send('User already exists')
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    const newUser = await db.register_user([email, hash])
+
+    req.session.user = newUser[0]
+
+    res.status(200).send(req.session.user)
   },
-  logout: () => {
-    //TODO Logout user
+  logout: (req, res) => {
+    req.session.destroy()
+    res.sendStatus(200)
   },
-  getUser: () => {
-    //TODO Get user from session
+  getUser: (req, res) => {
+    if (req.session.user) {
+      res.status(200).send(req.session.user)
+    } else {
+      res.sendStatus(404)
+    }
   },
 }
